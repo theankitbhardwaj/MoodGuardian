04d64e9b28f76b5b11069ff3889651100c0bc3fd
theankitbhardwaj <iamankitbhardwaj@gmail.com>
2023-01-23 22:50:10 +0530

    Initial Commit

---
+++ src/main/java/com/bhardwaj/MoodGuardian/MoodGuardianApplication.java
@@ -0,0 +1,24 @@
+package com.bhardwaj.MoodGuardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@SpringBootApplication
+@RestController
+public class MoodGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodGuardianApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<Optional<String>> home(){
+		return new ResponseEntity<>(Optional.of("Hello! Welcome to MoodGuardian."),HttpStatus.OK);
+	}
+}
---
+++ src/test/java/com/bhardwaj/MoodGuardian/MoodGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhardwaj.MoodGuardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
